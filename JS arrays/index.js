36323c14f3b3534ac0de1d6c7dd5e21058cb7c8b
Ridham Kansara <ridhamkansara26@gmail.com>
2025-04-25 18:25:28 +0530

    regular array methods

--- JS arrays/index.js
+++ JS arrays/index.js
@@ -43,5 +43,60 @@ console.log(fruites);
 console.log(fruites.push("Orange"))
 console.log(fruites);
 
+//7) Array shift() 
+
+console.log(fruites.shift());
+console.log(fruites);
+
+//8) Array unshift()
+
+console.log(fruites.unshift("Banana"))
+console.log(fruites)
+
+//9) Array Delete
+
+const vegies = ["tomato","potato","onion","cucumber"]
+// delete vegies[0]
+console.log(vegies);
+
+
+//10) Array concat()
+
+const arr1 = ["value1","value2"]
+const arr2 = ["value3","value4"]
+
+let mergeArr = arr1.concat(arr2);
+console.log(mergeArr);
+
+const arr3 = ["value5","value6"]
+
+mergeArr = arr1.concat(arr2,arr3)  //for any values
+console.log(mergeArr)
+
+
+
+//11) Array copywithin()
+
+console.log(vegies)
+vegies.copyWithin(2,0,2)
+console.log(vegies)
+
+
+//12)Array flat
+
+const myArr = [[1,2],[3,4],[5,6]]
+console.log(myArr.flat())
+
+
+//13)Array splice
+
+fruites.splice(2,0,"Lemon");
+console.log(fruites)
+
+
+//14) Array slice
+
+let removed = fruites.slice(2);
+console.log(removed);
 
 
