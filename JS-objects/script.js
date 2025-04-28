dc4da5d3227e1c6392041c9265c6962e69931fcd
Ridham Kansara <ridhamkansara26@gmail.com>
2025-04-28 18:07:46 +0530

    object and array destructuring

--- JS-objects/script.js
+++ JS-objects/script.js
@@ -26,6 +26,7 @@ const person2 = {firstName: "Anne", LastName: "Smith"};
 Object.assign(person,person2);
 
 console.log(person);
+console.log(person2);
 
 
 //2)Object create
@@ -79,5 +80,85 @@ let frozonObj = Object.freeze(details);
 details.firstName = "newName";
 console.log("original",details); //No change 
 console.log(frozonObj);
+
+
+//Deep Copy and Shallow Copy
+
+let employee = {
+    eid: "E101",
+    ename: "Ronak",
+    salary: 50000
+}
+
+
+//shallow copy
+console.log("Employee=====>",employee)
+
+const newEmployee = employee
+console.log("newEmployee===>",newEmployee)
+
+console.log("after changes=====>")
+
+newEmployee.ename = "Rahul";
+
+console.log("Employee=====>",employee)
+console.log("newEmployee===>",newEmployee)
+
+//deepCopy
+
+const employee2 = {
+    eid: "E201",
+    ename: "Mahesh",
+    salary: 20000
+}
+
+
+const newEmp = JSON.parse(JSON.stringify(employee2));
+console.log("Initial:",employee2);
+console.log("Copy Initial:",newEmp);
+
+console.log("After modification===>");
+newEmp.ename = "Mansi"
+
+console.log(employee2);
+console.log(newEmp);
+
+
+
+//Object and Array destructuring:
+
+
+//-Arrays Destructuring
+
+let intro = ["Hello","I","am","Ridham"];
+let [greet,pronoun,,name] = intro;
+
+console.log(greet);
+console.log(pronoun);
+console.log(name)
+
+
+//using rest
+let [greeting, ...allvalues] = intro;
+console.log(greeting);
+console.log(allvalues);
+
+
+
+//=Object destructuring
+
+const newPerson = {profileName: "Sara", country: "India", 
+                  job: "Developer", address: {street: "xyz", area: "local"}}
+
+const {profileName,country,job, address : {street , area}} = newPerson
+
+console.log(profileName)
+console.log(country)
+console.log(job)
+console.log(street)
+console.log(area)
+
+
+
 
 
