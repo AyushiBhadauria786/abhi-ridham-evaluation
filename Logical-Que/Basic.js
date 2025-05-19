ba4b2e63871436e3bb20b5888389c790e114dba7
mrbutani <abhibutani2592@gmail.com>
2025-05-19 13:19:01 +0530

    Count Vowels in String

--- Logical-Que/Basic.js
+++ Logical-Que/Basic.js
@@ -123,3 +123,34 @@ console.log(maxi);
 
 
 
+
+
+// --------------------------------------------------------------------------------------------------------
+
+
+
+
+
+// Count Vowels in a String
+
+
+let str2 = "abhi butani";
+
+
+
+// using for loop 
+
+let count = 0;
+for (let char of str2.toLowerCase()) {
+  if ('aeiou'.includes(char)) {
+    count++;
+  }
+}
+
+console.log(count);
+
+
+
+
+const count1 = (str2.match(/[aeiou]/gi) || []).length;
+console.log(count1)
