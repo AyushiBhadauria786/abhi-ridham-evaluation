c6360e620516a1b42d13a1422347dc3acb7cc935
mrbutani <abhibutani2592@gmail.com>
2025-05-19 12:20:28 +0530

    Palindrome string

--- Logical-Que/Basic.js
+++ Logical-Que/Basic.js
@@ -36,3 +36,48 @@ function twopointer(str){
 }
 
 console.log(twopointer(str));
+
+
+
+
+// -------------------------------------------------------------------------------------------------------
+
+
+
+
+
+// Check for Palindrome
+
+let str1 = "level"
+
+
+// Compare to reversed string
+
+const ispalindrome = str1 === str1.split('').reverse().join('');
+console.log(ispalindrome);
+
+
+// string is palindrome or not
+
+function checkPalindrome(string) {
+    const len = string.length;
+    
+    for (let i = 0; i < len / 2; i++) {
+        console.log(string[i])
+       
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+const string = prompt('Enter a string: ');
+const value = checkPalindrome(string);
+
+console.log(value);
+
+
+
+
+// --------------------------------------------------------------------------------------------
