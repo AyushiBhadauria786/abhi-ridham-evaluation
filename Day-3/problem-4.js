0543dff0be6e925951859f0bd9669cf19ce03d67
Ridham Kansara <ridhamkansara26@gmail.com>
2025-05-21 13:05:46 +0530

    capitalize first letter of sentence for every world

---
+++ Day-3/problem-4.js
@@ -0,0 +1,37 @@
+//Capitalise the First Letter of Each Word in a Sentence in JavaScript.
+
+
+//Solution-1
+
+let sentence = "my name is ridham kansara";
+let sentence2 = "check for the value"
+
+
+function capitalize(str){
+    let arr = str.split(" ");
+
+    for(let i=0; i < arr.length; i++){
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+    }
+   return arr.join(' ');
+
+}
+
+let result = capitalize(sentence);
+console.log(result);
+
+console.log(capitalize(sentence2));
+
+
+//Solution 2
+
+function capitalFirst(str){
+    let splitValue =  str.toLowerCase().split(' ');
+    let result = splitValue.map((value) => {
+        return (value.charAt(0).toUpperCase() + value.slice(1));
+    }).join(' ');
+    return result;
+}
+
+console.log(capitalFirst(sentence));    
+
