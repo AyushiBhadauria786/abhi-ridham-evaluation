935a3e0e3c51a545bd94a56e5da8e5fce47cb853
Ridham Kansara <ridhamkansara26@gmail.com>
2025-05-22 13:56:24 +0530

    added other remove duplicate methods

--- Day-4/problem-4.js
+++ Day-4/problem-4.js
@@ -3,6 +3,8 @@
 
 //Solution 1
 
+//using set we first add all array elements in set and then convert Set to array and return that. 
+
 function removeDuplicate(arr){
     let set = new Set(arr);
     let newArr = Array.from(set);
@@ -12,3 +14,30 @@ function removeDuplicate(arr){
 
 const dupArr = [1,1,4,4,2,2,3,3,5,5];
 console.log(removeDuplicate(dupArr));
+
+
+//Solution 2
+
+//using set and spread operator
+
+let values = [10,20,10,20,30,30,50,40,40];
+
+let set = new Set(values);
+
+let removedValue = [...set];
+console.log(removedValue);
+
+
+//Solution 3
+
+//using forEach and includes method
+
+let a1 = [];
+
+values.forEach((element) =>{
+    if(!a1.includes(element)){
+        a1.push(element);
+    }
+});
+
+console.log(a1);
