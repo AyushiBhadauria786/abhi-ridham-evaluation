87d5d3ce540b21c14b06654804d34b36e734ef88
mrbutani <abhibutani2592@gmail.com>
2025-05-26 13:03:46 +0530

    Flatten a nested object

---
+++ Logical-Que/Day-6/Problem-3.js
@@ -0,0 +1,50 @@
+// Transforming a deeply nested JavaScript object into a flat single-level object with concatenated keys (Flatten a nested object)
+
+
+
+// Using Recursive Approach
+
+
+const obj = {
+    a : 3,
+    b : 4,
+    c : {
+        d:5,
+        e:6
+    }
+}
+
+
+function flattenObject(obj, parentKey = '', res = {}) {
+    for (let key in obj) {
+      let newKey = parentKey ? `${parentKey}.${key}` : key;
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        flattenObject(obj[key], newKey, res);
+      } else {
+        res[newKey] = obj[key];
+      }
+    }
+    return res;
+  }
+  
+  console.log(flattenObject(obj));
+
+
+
+
+  // Using reducer 
+
+
+  function flattenObjectReduce(obj) {
+    return Object.keys(obj).reduce((acc, key) => {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        Object.assign(acc, flattenObjectReduce(obj[key]));
+      } else {
+        acc[key] = obj[key];
+      }
+      return acc;
+    }, {});
+  }
+
+  
+  console.log(flattenObjectReduce(obj));
