155418064f12e8256984f0bc231fe393d65e46e5
mrbutani <abhibutani2592@gmail.com>
2025-05-28 17:19:19 +0530

    Sort an array using the selection sort technique.

---
+++ Logical-Que/Day-8/Problem-8.js
@@ -0,0 +1,22 @@
+// Sort an array using the selection sort technique.
+
+
+let arr = [8,4,1,6,9,3,7,2,5];
+
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+    }
+    return arr;
+  }
+
+  console.log(selectionSort(arr));
