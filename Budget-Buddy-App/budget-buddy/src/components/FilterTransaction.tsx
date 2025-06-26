6b21bcafac051472d98c392b273e8c9140d4c480
Ridham Kansara <ridhamkansara26@gmail.com>
2025-06-26 22:18:19 +0530

    added filter component for transactions

---
+++ Budget-Buddy-App/budget-buddy/src/components/FilterTransaction.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+
+
+const FilterTransaction = () => {
+  return (
+    <Box>
+        <Typography variant="h5" sx={{fontSize: "24px", lineHeight: "24px"}}>Filters</Typography>
+    </Box>
+  )
+}
+
+export default FilterTransaction
--- Budget-Buddy-App/budget-buddy/src/pages/Transactions.tsx
+++ Budget-Buddy-App/budget-buddy/src/pages/Transactions.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
-
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import FilterTransaction from "../components/FilterTransaction";
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
-  )
-}
+    <Box sx={{ padding: "24px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          boxSizing: "border-box",
+        }}
+      >
+        <Typography variant="h5" sx={{fontWeight: 700, fontSize: "24px"}}>Transaction History</Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "rgb(71 85 105)",
+            fontWeight: 600,
+            lineHeight: "32px",
+            ":hover": {
+              backgroundColor: "rgb(55 65 81)",
+            },
+          }}
+        >
+          Add Transaction
+        </Button>
+      </Box>
+      <Box sx={{marginTop: 2}}>
+          <FilterTransaction />
+      </Box>
+    </Box>
+  );
+};
 
-export default Transactions
+export default Transactions;
